576a70980c2b18d6f964167e56c679233c806fd4
andreaspersson <andreas.persa.persson@gmail.com>
2017-06-19 12:50:57 +0200

    add github adress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import {MdCard} from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app works!';
 }
+
+
+//** Begin *
+let str = '';
+let a = [ 72, 105, 114, 101, 32, 
+          65, 110, 100, 114, 101,
+          97, 115, 33
+        ];
+
+for(let i in a){
+ str += String.fromCharCode(a[i]);
+}
+
+console.log(str)
+//** End *
